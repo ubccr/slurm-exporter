3349a5dc2dfb62c56139a4486a5019d039ed2d0f
Trey Dockendorf <tdockendorf@osc.edu>
2021-10-12 11:03:27 -0400

    Support queries to TCP based slurmrestd

--- main.go
+++ main.go
@@ -18,9 +18,16 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"fmt"
 	"net"
 	"net/http"
+	"net/url"
+	"os/exec"
+	"os/user"
+	"regexp"
+	"strings"
 	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
@@ -30,47 +37,111 @@ import (
 	kingpin "gopkg.in/alecthomas/kingpin.v2"
 )
 
+var (
+	listenAddress = kingpin.Flag(
+		"listen-address",
+		"Address on which to expose metrics and web interface.",
+	).Default(":9122").String()
+
+	unixSocket = kingpin.Flag(
+		"unix-socket",
+		"Path to slurmrestd unix socket.",
+	).Default("/tmp/slurmrestd.sock").String()
+
+	restURL = kingpin.Flag(
+		"rest-url",
+		"SLURM REST API URL, eg http://localhost:6820",
+	).Default("").String()
+
+	timeout      = kingpin.Flag("rest-timeout", "Timeout of REST HTTP queries").Default("60").Int()
+	tokenTimeout = kingpin.Flag("token-timeout", "Timeout of getting REST token").Default("5").Int()
+	debug        = kingpin.Flag("debug", "enable debug mode").Default("false").Bool()
+)
+
 func main() {
-	var (
-		listenAddress = kingpin.Flag(
-			"listen-address",
-			"Address on which to expose metrics and web interface.",
-		).Default(":9122").String()
-
-		unixSocket = kingpin.Flag(
-			"unix-socket",
-			"Path to slurmrestd unix socket.",
-		).Default("/tmp/slurmrestd.sock").String()
-
-		debug = kingpin.Flag("debug", "enable debug mode").Default("false").Bool()
-	)
-
 	kingpin.HelpFlag.Short('h')
 	kingpin.Parse()
 
 	if *debug {
 		log.SetLevel(log.DebugLevel)
 	} else {
-		log.SetLevel(log.WarnLevel)
+		log.SetLevel(log.InfoLevel)
 	}
 
-	tr := &http.Transport{
-		DialContext: func(ctx context.Context, _, addr string) (net.Conn, error) {
-			dialer := net.Dialer{}
-			return dialer.DialContext(ctx, "unix", *unixSocket)
-		},
-	}
 	cfg := slurmrest.NewConfiguration()
-	cfg.HTTPClient = &http.Client{Timeout: time.Second * 3600, Transport: tr}
-	cfg.Scheme = "http"
-	cfg.Host = "localhost"
-
-	client := slurmrest.NewAPIClient(cfg)
-	prometheus.MustRegister(NewNodesCollector(client))
-	prometheus.MustRegister(NewSchedulerCollector(client))
-	prometheus.MustRegister(NewJobsCollector(client))
+	cfg.HTTPClient = &http.Client{Timeout: time.Duration(*timeout) * time.Second}
+	if *restURL != "" {
+		url, err := url.Parse(*restURL)
+		if err != nil {
+			log.Fatal(err)
+		}
+		cfg.Scheme = url.Scheme
+		cfg.Host = url.Host
+		user, err := user.Current()
+		if err != nil {
+			log.Fatal(err)
+		}
+		headers := make(map[string]string)
+		headers["X-SLURM-USER-NAME"] = user.Username
+		headers["X-SLURM-USER-TOKEN"] = ""
+		cfg.DefaultHeader = headers
+	} else {
+		tr := &http.Transport{
+			DialContext: func(ctx context.Context, _, addr string) (net.Conn, error) {
+				dialer := net.Dialer{}
+				return dialer.DialContext(ctx, "unix", *unixSocket)
+			},
+		}
+		cfg.Scheme = "http"
+		cfg.Host = "localhost"
+		cfg.HTTPClient.Transport = tr
+	}
 
 	log.Infof("Starting Server: %s", *listenAddress)
-	http.Handle("/metrics", promhttp.Handler())
+	http.Handle("/metrics", metricsHandler(cfg))
 	log.Fatal(http.ListenAndServe(*listenAddress, nil))
+}
+
+func metricsHandler(cfg *slurmrest.Configuration) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if *restURL != "" {
+			token, err := getToken()
+			if err != nil {
+				http.Error(w, fmt.Sprintf("Unable to get JWT: %s", err), http.StatusNotFound)
+				return
+			}
+			cfg.DefaultHeader["X-SLURM-USER-TOKEN"] = token
+		}
+		client := slurmrest.NewAPIClient(cfg)
+
+		registry := prometheus.NewRegistry()
+		registry.MustRegister(NewNodesCollector(client))
+		registry.MustRegister(NewSchedulerCollector(client))
+		registry.MustRegister(NewJobsCollector(client))
+		gatherers := prometheus.Gatherers{registry, prometheus.DefaultGatherer}
+		h := promhttp.HandlerFor(gatherers, promhttp.HandlerOpts{})
+		h.ServeHTTP(w, r)
+	}
+}
+
+func getToken() (string, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(*tokenTimeout)*time.Second)
+	defer cancel()
+	cmd := exec.CommandContext(ctx, "scontrol", "token")
+	var stdout, stderr bytes.Buffer
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+	err := cmd.Run()
+	if err != nil {
+		return "", err
+	}
+	re := regexp.MustCompile(`^SLURM_JWT=(.*)`)
+	match := re.FindStringSubmatch(strings.TrimSpace(stdout.String()))
+	var token string
+	if len(match) == 2 {
+		token = match[1]
+	} else {
+		return "", fmt.Errorf("Unable to match SLURM_JWT from output, output=%s", stdout.String())
+	}
+	return token, nil
 }
